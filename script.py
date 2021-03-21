81071b0d769bd4fbdbd76502fd1178abc713a2d2
lucas2kdk <lucasmindworld@gmail.com>
2021-03-21 21:49:12 +0100

    Commit

--- script.py
+++ script.py
@@ -3,37 +3,46 @@ import time
 import playsound
 import speech_recognition as sr
 from gtts import gTTS
+import glob
 
+
+### Program Init ###
+
+for filepath in glob.iglob(r'commands\*.py'):
+    print(filepath)
+
+### Standard Settings ###
 programActive = True
 microphoneMuted = False
 
-while programActive == True:
-	while microphoneMuted == False:
-		def speak(text):
-		    tts = gTTS(text=text, lang="en")
-		    filename = "voice.mp3"
-		    tts.save(filename)
-		    playsound.playsound(filename)
-		    os.remove(filename)
+def Listener():
+	while programActive == True:
+		while microphoneMuted == False:
+			def speak(text):
+				tts = gTTS(text=text, lang="en")
+				filename = "voice.mp3"
+				tts.save(filename)
+				playsound.playsound(filename)
+				os.remove(filename)
 
 
-		def get_audio():
-		    r = sr.Recognizer()
-		    with sr.Microphone() as source:
-		        audio = r.listen(source)
-		        said = ""
+			def get_audio():
+				r = sr.Recognizer()
+				with sr.Microphone() as source:
+					audio = r.listen(source)
+					said = ""
 
-		        try:
-		            said = r.recognize_google(audio)
-		            print(said)
-		        except Exception as e:
-		            print("Exception: " + str(e))
+					try:
+						said = r.recognize_google(audio)
+						print(said)
+					except Exception as e:
+						print("Exception: " + str(e))
 
-		    return said
+				return said
 
-		text = get_audio()
+			text = get_audio()
 
-		if "hello" in text:
-		    speak("hello, how are you?")
-		elif "what is your name" in text:
-		    speak("My name is Axel")
+			if "hello" in text:
+				speak("hello, how are you?")
+			elif "what is your name" in text:
+				speak("My name is Axel")
